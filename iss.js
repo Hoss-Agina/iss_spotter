3c228b627802d738fc7ca821e53a3a5829de8906
Hoss Agina <hoss.agina@telus.com>
2022-11-24 20:35:56 -0700

    added fetchCoordersByIP

--- iss.js
+++ iss.js
@@ -1,6 +1,6 @@
 const request = require("request");
 
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   request('https://api.ipify.org/?format=json', (error, response, body) => {
     if (error) {
       callback(error, null);
@@ -17,5 +17,23 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function(ipAdd, callback) {
+  request(`http://ipwho.is/${ipAdd}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    let coordinates = {};
+    const dataObj = JSON.parse(body);
+    if (!dataObj.success) {
+      const message = `Success status was ${dataObj.success}. Server message says: ${dataObj.message} when fetching for IP ${dataObj.ip}`;
+      callback(message, null);
+      return;
+    }
+    coordinates["latitude"] = dataObj["latitude"];
+    coordinates["longitude"] = dataObj["longitude"];
+    callback(null, coordinates);
+  });
+};
 
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordsByIP};
